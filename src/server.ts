2aed8b49c6fc810c0ba92516218fe5e9a8b795df
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-08-19 10:27:20 +0900

    server.tsへ変更

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app: express.Application = express();
-const port: string | number = process.env.PORT || 3000;
+const port: string | number = process.env.PORT || 8080;
 
 app.get('/', (req: express.Request, res: express.Response) => res.send('Hello World!'));
 
